ece35c21e4ff9e2063e7212706ea48e91b89d8cb
cooldeark <45022034+cooldeark@users.noreply.github.com>
2018-11-20 15:07:52 +0800

    Create PHP_Objects.php

---
+++ PHP_Objects.php
@@ -0,0 +1,8 @@
+    $hostname="localhost";
+    $username="test";
+    $password="pass"
+    $database="mydata";
+    
+    $myObject=new stdClass();//build a object
+    $data=["hostname"=>$hostname,"username"=>$username,"password"=>$password,"database"=>$database];
+    $myObject->property=$data;
