c53cf95aee4fd0e1b26494bee98f8728a6b18dd0
cooldeark <45022034+cooldeark@users.noreply.github.com>
2018-11-20 15:19:23 +0800

    Update PHP_Objects.php

--- PHP_Objects.php
+++ PHP_Objects.php
@@ -1,8 +1,11 @@
+<?php   
+    /*Defined variables*/
     $hostname="localhost";
     $username="test";
     $password="pass"
     $database="mydata";
-    
+   
     $myObject=new stdClass();//build a object
     $data=["hostname"=>$hostname,"username"=>$username,"password"=>$password,"database"=>$database];
     $myObject->property=$data;
+??
